c4de0a7f293fa47403ae6e2f3649c9d8ff59dabf
goit <goit@gmail.com>
2026-01-17 13:13:33 +0700

    initial commit

---
+++ src/components/Home.vue
@@ -0,0 +1,11 @@
+<script setup>
+    import image1 from '@/assets/images/image1.webp'
+
+</script>
+
+<template>
+    <div class="flex justify-center bg-red-300 w-full p-4">
+        <img :src="image1" alt="image-1" class="w-[250px]">
+    </div>
+    <h3 class="font-bold text-xl">Halo nama saya Ogi</h3>
+</template>
